811381180e0e0d2e9199906d4063597b607bef52
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-09-03 19:33:40 +0100

    Initialized Main SDL Window Function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
-#include <SDL.h>
-#include <stdio.h>
+#include "../inc/raycaster.h"
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
+int main(void)
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    if (SDL_Init())
+}
